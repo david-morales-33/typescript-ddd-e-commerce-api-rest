f8cc08d9c518178a009a7cdf2ffd5dd158f8f86d
david-morales-33 <davidmorales33.97@gmail.com>
2024-12-12 23:07:34 -0500

    Integracion de entidad <LabelDTO> en contexto <catalog>

--- src/contexts/Catalog/Label/domain/Label.ts
+++ src/contexts/Catalog/Label/domain/Label.ts
@@ -1,3 +1,4 @@
+import { LabelDTO } from "./LabelDTO";
 import { LabelId } from "./LabelId";
 import { LabelValue } from "./LabelValue";
 
@@ -6,4 +7,18 @@ export class Label {
         public readonly id: LabelId,
         public readonly value: LabelValue
     ) { }
+
+    public static fromPrimitives(data: LabelDTO): Label {
+        return new Label(
+            new LabelId(data.id),
+            new LabelValue(data.value)
+        )
+    }
+
+    public toString(): LabelDTO {
+        return new LabelDTO(
+            this.id.value,
+            this.value.value
+        );
+    }
 }
---
+++ src/contexts/Catalog/Label/domain/LabelDTO.ts
@@ -0,0 +1,7 @@
+
+export class LabelDTO {
+    constructor(
+        public readonly id: string,
+        public readonly value: string
+    ){}
+}
