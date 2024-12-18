1b464c1b1492a2d2176658c5ac645b9c16b336e7
david-morales-33 <davidmorales33.97@gmail.com>
2024-12-17 21:33:10 -0500

    Modificacion de entidad <Price> en contexto <catalog>

--- src/contexts/Catalog/Price/domain/Price.ts
+++ src/contexts/Catalog/Price/domain/Price.ts
@@ -1,4 +1,5 @@
 import { PriceCurrency } from "./PriceCurrency";
+import { PriceDTO } from "./PriceDTO";
 import { PriceFinalDate } from "./PriceFinalDate";
 import { PriceId } from "./PriceId";
 import { PriceInitialDate } from "./PriceInitialDate";
@@ -9,7 +10,27 @@ export class Price {
         public readonly id: PriceId,
         public readonly value: PriceValue,
         public readonly currency: PriceCurrency,
-        public readonly inicialDate: PriceInitialDate,
+        public readonly initialDate: PriceInitialDate,
         public readonly finalDate: PriceFinalDate
     ) { }
+
+    public static fromPrimitives(data: PriceDTO): Price {
+        return new Price(
+            new PriceId(data.id),
+            new PriceValue(data.value),
+            PriceCurrency.fromValue(data.currency),
+            new PriceInitialDate(data.initialDate),
+            new PriceFinalDate(data.finalDate)
+        )
+    }
+
+    public toPrimitives(): PriceDTO {
+        return new PriceDTO(
+            this.id.value,
+            this.value.value,
+            this.currency.value,
+            this.initialDate.value,
+            this.finalDate.value
+        )
+    }
 }
--- src/contexts/Catalog/Price/domain/PriceDTO.ts
+++ src/contexts/Catalog/Price/domain/PriceDTO.ts
@@ -1,13 +1,10 @@
 
-
 export class PriceDTO {
     constructor(
         public readonly id: string,
-        public readonly value: string,
+        public readonly value: number,
         public readonly currency: string,
-        public readonly inicialDate: Date,
+        public readonly initialDate: Date,
         public readonly finalDate: Date
-    ) {
-
-    }
+    ) { }
 }
